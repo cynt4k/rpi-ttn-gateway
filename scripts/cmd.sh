a93f44265f98515ddf767b9863c7546c76159a5b
cynt4k <me@cynt4k.de>
2018-10-27 22:43:36 +0100

    Modified for own usage

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Reset RAK831
+echo "17" > /sys/class/gpio/export 
+echo "out" > /sys/class/gpio/gpio17/direction 
+echo "1" > /sys/class/gpio/gpio17/value 
+sleep 5 
+echo "0" > /sys/class/gpio/gpio17/value 
+sleep 1 
+echo "0" > /sys/class/gpio/gpio17/value 
+sleep 3
+
 # change reset pin assignment if configured and not default
 defaultResetPin=25
 if [ -z "$PI_RESET_PIN" ]; then
